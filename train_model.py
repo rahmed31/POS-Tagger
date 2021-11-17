0715baa8aa6cdd1f31985c9cc7162f9543be94a6
Raihan Ahmed <raihan.ahmed117@gmail.com>
2021-11-17 03:32:22 -0600

    Refactored code for better modularization and understandability

--- train_model.py
+++ train_model.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------------------
-#The purpose of this code is to provide the output of the model that's created when using
-#the Brown training corpus (data/train_corpus.txt) to train this Part of Speech (POS) tagger.
-#The model is written into a text file, which is subsequently saved to the data folder as model_file.txt.
-#This allows for easy visualization of what a Trigram HMM model could look like when creating
-#your own POS tagger!
+#The purpose of this code is to provide the model output that's created when using the Brown training
+#corpus (data/train_corpus.txt) to train a Part of Speech (POS) tagger. The model is written into a
+#text file, which is subsequently saved to the data folder as model_file.txt. The purpose of the text file
+#is to provide an easy visualization of what a Trigram HMM model could look like when creating your own POS tagger!
+#Instead of using model_file.txt in further applications, the unigram, bigram, trigram, and emission dictionaries
+#will be pickled to provide quick and easy access for the POS tagger.
+#
 #
 #(C) 2021 Raihan Ahmed, Chicago, IL
 #Released under MIT License
@@ -15,62 +17,57 @@
 import sys
 import re
 import os
+import pickle
 
 START_SYMBOL = '*'
-STOP_SYMBOL = ' STOP'
+STOP_SYMBOL = 'STOP'
 
 #get ngrams in tuple form
-def _pos_sequences(sentences, n):
-    """ Function to find pos sequences of length 'n'. It returns a dictionary
-        containing the corpus' pos sequences and their frequencies. This function specifically caters to files
+def pos_ngram(taglists, n):
+    """ Function to find pos ngrams of length 'n'. It returns a dictionary containing the corpus'
+        pos sequences in tuple form and their frequencies. This function specifically caters to files
         with the format of the Brown corpus. Runtime complexity: O(n^2) """
 
     tag_sequences = {}
 
-    for sentence in sentences:
+    for taglist in taglists:
         #add n - 1 star symbols to the beginning of the list to account for ngrams of different size
-        tags_list = ((n - 1) * [START_SYMBOL])
+        taglist = ((n - 1) * [START_SYMBOL] + taglist + [STOP_SYMBOL])
 
-        for word in sentence.split(" "):
-            pos = word.rsplit('/', 1)[1]
-            tags_list.append(pos)
+        for i in range(len(taglist) - n + 1):
+            #creating ngram tuple from taglist
+            ngram  = tuple(taglist[i:i+n])
 
-        for i in range(len(tags_list) - n + 1):
-            #joining pos tags in tags_list to create a single tag sequence
-            sequence = ' '.join(tags_list[i:i+n])
-
-            if sequence in tag_sequences:
-                tag_sequences[sequence] += 1
+            if ngram in tag_sequences:
+                tag_sequences[ngram] += 1
             else:
-                tag_sequences.update({sequence : 1})
+                tag_sequences.update({ngram : 1})
 
     #sort tag_sequences dictionary from greatest count to lowest count
     sorted_tag_sequences = sorted(tag_sequences.items(), key=lambda x: x[1], reverse = True)
 
     return sorted_tag_sequences
 
-def _emissions(sentences):
+def emissions(tokenlists, taglists):
     """ Function to find emission counts for each word and their associated POS tag. It returns a dictionary
-        containing the corpus' emission counts for each word. This function specifically caters to files
+        containing the corpus' emission counts for each token/tag tuple. This function specifically caters to files
         with the format of the Brown corpus. Runtime complexity: O(n^2) """
 
     emissions = {}
 
-    for sentence in sentences:
-
-        for word in sentence.split(" "):
-
-            if word in emissions:
-                emissions[word] += 1
+    for tokenlist, taglist in zip(tokenlists, taglists):
+        for token, tag in zip(tokenlist, taglist):
+            if (token, tag) in emissions:
+                emissions[(token, tag)] += 1
             else:
-                emissions.update({word : 1})
+                emissions.update({(token, tag) : 1})
 
     #sort tag_sequences dictionary from greatest count to lowest count
-    sorted_emissions = sorted(emissions.items(), key=lambda x: x[1], reverse = True)
+    sortedemissions = sorted(emissions.items(), key=lambda x: x[1], reverse = True)
 
-    return sorted_emissions
+    return sortedemissions
 
-def _clean_text(training_corpus):
+def clean_text(training_corpus):
     """ Function used for cleaning text from training data that follows the format of the Brown corpus. Closed
         category words and punctuation are not removed to be able to ensure that training sentences are
         syntactically sound. The first return value is a list that contains distinct lists holding the tokens
@@ -93,50 +90,45 @@ def _clean_text(training_corpus):
         lines = [re.sub(' +', ' ', line) for line in lines]
 
         tokenlists = [[wordtag.rsplit('/', 1)[0] for wordtag in line.strip().split(" ")] for line in lines]
-        #Add start symbol to each list twice to account for trigram HMM, and add STOP_SYMBOL to mark the end of each sentence
-        tokenlists = [[START_SYMBOL] * 2 + tokenlist + [STOP_SYMBOL] for tokenlist in tokenlists]
 
         taglists = [[wordtag.rsplit('/', 1)[-1] for wordtag in line.strip().split(" ")] for line in lines]
-        taglists = [[START_SYMBOL] * 2 + taglist + [STOP_SYMBOL] for taglist in taglists]
 
         f.close()
 
         return tokenlists, taglists
 
-def create_model_file(training_corpus):
-    """ Function for creating trigram hidden Markov model using unigrams, bigrams, trigrams,
-        word tag sequences, and their counts. The model is written onto a file in the library"""
-
-    tokenlists, taglists = _clean_text(training_corpus)
+def create_model_file(tokenlists, taglists):
+    """ Function for creating trigram hidden Markov model using POS unigrams, bigrams, trigrams, and
+        word/tag pairs with their emissions. The model is written onto a file in the library. The text file
+        will not be used beyond this point. The purpose of the model text file """
 
     tag_size = 0
     tag_total = 0
 
-    #need to redesign this block
     with open('data/model_file.txt', 'w+') as f:
         #clear file if contents already exist
         f.truncate(0)
 
         f.write('@unigrams@' + '\n')
-        for key, value in _pos_sequences(input_text, 1):
+        for key, value in pos_ngram(taglists, 1):
             tag_size += 1
             tag_total += value
-            string = key + '\t' + str(value)
+            string = str(key) + '\t' + str(value)
             f.write(string + '\n')
 
         f.write('@bigrams@' + '\n')
-        for key, value in _pos_sequences(input_text, 2):
-            string = key + '\t' + str(value)
+        for key, value in pos_ngram(taglists, 2):
+            string = str(key) + '\t' + str(value)
             f.write(string + '\n')
 
         f.write('@trigrams@' + '\n')
-        for key, value in _pos_sequences(input_text, 3):
-            string = key + '\t' + str(value)
+        for key, value in pos_ngram(taglists, 3):
+            string = str(key) + '\t' + str(value)
             f.write(string + '\n')
 
         f.write('@emissions@' + '\n')
-        for key, value in _emissions(input_text):
-            string = key + '\t' + str(value)
+        for key, value in emissions(tokenlists, taglists):
+            string = str(key) + '\t' + str(value)
             f.write(string + '\n')
 
         #tag_size is not necessarily the same as vocab size, but tag_total will always
@@ -153,6 +145,18 @@ def create_model_file(training_corpus):
 #Main driver used for debugging
 if __name__ == '__main__':
 
-    _clean_text('data/train_corpus.txt')
+    tokenlists, taglists = clean_text('data/train_corpus.txt')
+
+    create_model_file(tokenlists, taglists)
+
+    unigrams = pos_ngram(taglists, 1)
+    bigrams = pos_ngram(taglists, 2)
+    trigrams = pos_ngram(taglists, 3)
+    emissions = emissions(tokenlists, taglists)
+
+    pickle.dump(unigrams, open("data/unigrams.pickle", "wb" ))
+    pickle.dump(bigrams, open("data/bigrams.pickle", "wb" ))
+    pickle.dump(trigrams, open("data/trigrams.pickle", "wb" ))
+    pickle.dump(emissions, open("data/emissions.pickle", "wb" ))
 
 ##########################################################################################################
