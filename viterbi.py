bea1b8c4bc8027551097810c319c92b526222042
Raihan Ahmed <raihan.ahmed117@gmail.com>
2021-11-22 16:41:26 -0600

    moved deleted inter, will need to determine lambda values experimentally

--- viterbi.py
+++ viterbi.py
@@ -13,6 +13,46 @@ import math
 import time
 import pickle
 
+#this method is called by transition_probs() to approximate probabilities for unseen trigrams
+#needs fix
+def deleted_interpolation(unigrams, bigrams, trigrams):
+    lambda1 = 0
+    lambda2 = 0
+    lambda3 = 0
+
+    for a, b, c in trigrams.keys():
+        v = trigrams[(a, b, c)]
+
+        if v > 0:
+
+            try:
+                c1 = (v - 1) / (bigrams[(a, b)] - 1)
+            except:
+                c1 = 0
+            try:
+                c2 = (bigrams[(a, b)] - 1) / (unigrams[(a,)] - 1)
+            except:
+                c2 = 0
+            try:
+                c3 = (unigrams[(a,)] - 1) / (sum(unigrams.values()) - 1)
+            except:
+                c3 = 0
+
+            clist = [c1, c2, c3]
+            m = np.argmax(clist)
+
+            if m == 0:
+                lambda3 += v
+            if m == 1:
+                lambda2 += v
+            if m == 2:
+                lambda3 += v
+
+    weights = [lambda1, lambda2, lambda3]
+    weights = [a / sum(weights) for a in weights]
+
+    return weights
+
 def viterbi():
 
 
