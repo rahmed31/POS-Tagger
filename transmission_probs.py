fc44062e4f416e73601ee59e27f4496ec020a391
Raihan Ahmed <raihan.ahmed117@gmail.com>
2021-11-23 01:27:51 -0600

    Created hyperparameter tuning functions; updated description for file

--- transmission_probs.py
+++ transmission_probs.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 # # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------------------
-#The purpose of this file is to experimentally determine the lambda values that maximize the
-#accurracy of the POS tagger.
+#The purpose of this file is to calculate the transmission probabilities of all POS trigrams that occur in
+#the training corpus. These probabilities allow our model to predict the POS tags for the test corpus.
+#To remedy the issue of unseen POS trigrams that may result from the test corpus, interpolation is necessary to approximate
+#the probability of each POS trigram when calculating them. The deleted interpolation algorithm is
+#used to achieve this so that unknown POS tag sequences that may appear in the test corpus can still be predicted
+#with a non zero probability using lower order ngrams. Deleted interpolation also requires the use of lambda
+#values (i.e., lambda1, lambda2, lambda3) to help with maximizing the accuracy of the POS tagger when utilizing lower
+#order ngrams. These hyperparameters will be experimentally determined in this file such that the accuracy of the POS tagger is
+#maximized when using 10-fold cross validation on the training corpus.
 #
 #Copyright (C) 2021, released under MIT License
 #Author: Raihan Ahmed, Chicago, IL
@@ -17,21 +24,32 @@ from interpolations import replace_rare
 output_path = 'data/model_data/'
 LOG_ZERO = -1000
 
-def experimental_loop(taglists, unigrams, bigrams, trigrams):
+def find_lambdas(taglists, unigrams, bigrams, trigrams):
+    max_accuracy = 0
+    lambda_values = []
+    q_values = {}
+
     for i in range(0.001, 1, 0.001):
-        for j in range():
+        for j in range(0.001):
             for k in range():
-                q_values = transition_probs(taglists, unigrams, bigrams, trigrams)
+                log_values = transition_probs(taglists, unigrams, bigrams, trigrams, [i, j, k])
 
-                tags = viterbi(brown_dev_words, taglist, known_words, q_values, e_values)
+                # tags = viterbi(brown_dev_words, taglist, known_words, q_values, e_values)
+                #
+                # accuracy = calculate_accuracy(tags, brown_dev_words)
 
-                accuracy = calculate_accuracy(tags, brown_dev_words)
+                if accuracy > max_accuracy:
+                    max_accuracy = accuracy
+                    lambda_values = [i, j, k]
+                    q_values = log_values
 
-def calculate_accuracy(tags, brown_dev_words):
+    return lambda_values, q_values
+
+# def calculate_accuracy(tags, brown_dev_words):
 
 
 
-def transition_probs(taglists, unigrams, bigrams, trigrams):
+def transition_probs(taglists, unigrams, bigrams, trigrams, lambdas):
     unigram_total = sum(unigrams.values())
     unigram_p = {(a,): math.log(unigrams[(a,)], 2) - math.log(unigram_total, 2) for a, in unigrams}
 
@@ -41,10 +59,10 @@ def transition_probs(taglists, unigrams, bigrams, trigrams):
     bigrams[(START_SYMBOL, START_SYMBOL)] = len(taglists)
     trigram_p = {(a, b, c): math.log(trigrams[(a, b, c)], 2) - math.log(bigrams[(a, b)], 2) for a, b, c in trigrams}
 
-    #will need to experimentally determine lambda values by applying the viterbi algorithm in a for loop to maximize accuracy
-    trigram_d = {(a, b , c): math.log(0.3 * (2**trigram_p[(a, b, c)]) + 0.4 * (2**bigram_p[(b, c)]) + 0.3 * (2**unigram_p[(c,)]), 2) for a, b, c in trigram_p}
+    #calculating log transmission probabilities
+    q_values = {(a, b , c): math.log(lambdas[0] * (2**trigram_p[(a, b, c)]) + lambda[1] * (2**bigram_p[(b, c)]) + lambda[2] * (2**unigram_p[(c,)]), 2) for a, b, c in trigram_p}
 
-    return unigram_p, bigram_p, trigram_p, trigram_d
+    return q_values
 
 def viterbi(brown_dev_words, taglist, known_words, q_values, e_values):
     tagged = []
@@ -121,11 +139,14 @@ if __name__ == '__main__':
 
     tokenlists = pickle.load(open(output_path + "tokenlists.pickle", "rb" ))
     taglists = pickle.load(open(output_path + "taglists.pickle", "rb" ))
-
     e_values = pickle.load(open(output_path + "e_probs.pickle", "rb" ))
     known_words = pickle.load(open(output_path + "known_words.pickle", "rb" ))
     pos_set = pickle.load(open(output_path + "pos_set.pickle", "rb" ))
 
+    lambda_values, q_values = find_lambdas(taglists, unigrams, bigrams, trigrams)
+    # print(q_probs)
+
+    # q_probs = pickle.dump(q_probs, open(output_path + "q_probs.pickle", "wb" ))
 
     finish = time.perf_counter()
     print(f'Finished in {round(finish-start, 2)} second(s)')
