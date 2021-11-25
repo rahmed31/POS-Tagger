bb6c09c9d0cd4e364b876242d792e4c3e2a0fe9d
Raihan Ahmed <raihan.ahmed117@gmail.com>
2021-11-25 00:50:26 -0600

    Added function to calculate lambda values candidates

--- transmission_probs.py
+++ transmission_probs.py
@@ -19,49 +19,53 @@
 import math
 import time
 import pickle
+import numpy as np
 from viterbi import viterbi_algorithm
-from interpolations import replace_rare
+from emission_probs import replace_rare
+from train_model import clean_text
 
 output_path = 'data/model_data/'
 
-#INCOMPLETE FUNCTION
-def find_lambdas(taglists, unigrams, bigrams, trigrams):
+def lambda_candidates(start, end, step):
+    """ Function that generates all possible sets of lambda values that sum to 1 for deleted interpolation algorithm.
+        Runtime complexity: O(n^3)"""
+
+    l = np.arange(start, end, step)
+
+    result = []
+
+    for i in range(len(l1)):
+        for j in range(len(l1)):
+            for k in range(len(l1)):
+                if (l[i] + l[j] + l[k] == 1):
+                    result.append([l[i], l[j], l[k]])
+
+    return l
+
+def find_lambdas(candidate_values, test_set, taglists, unigrams, bigrams, trigrams, e_values):
     """ Function to experimentally determine the hyperparameters (i.e., lambda values) for the deleted interpolation
         algorithm. This function utilizes a 10-fold cross validation technique to determine which lambda values maximize
         the accuracy of the viterbi algorithm when determining the POS tags for the validation sets. It returns a list containing
         the lambda values and a dictionary containing the log transition probabilities for each POS trigram present in the training
-        corpus. This function specifically caters to files with the format of the Brown corpus. Runtime complexity: O(n^2) """
+        corpus. This function specifically caters to files with the format of the Brown corpus. Amortized runtime complexity: O(n) """
 
     max_accuracy = 0
     lambda_values = []
     q_values = {}
 
-    for i in range(0.001, 1, 0.001):
-        for j in range(0.001):
-            for k in range():
-                log_values = transition_probs(taglists, unigrams, bigrams, trigrams, [i, j, k])
+    for lambdas in candidate_values:
+        log_values = transition_probs(taglists, unigrams, bigrams, trigrams, lambdas)
+        tags = viterbi_algorithm(test_set, taglist, known_words, q_values, e_values)
+        accuracy = calculate_accuracy(tags, test_set)
 
-                # tags = viterbi_algorithm(brown_dev_words, taglist, known_words, q_values, e_values)
-                #
-                # accuracy = calculate_accuracy(tags, brown_dev_words)
-
-                if accuracy > max_accuracy:
-                    max_accuracy = accuracy
-                    lambda_values = [i, j, k]
-                    q_values = log_values
+        if accuracy > max_accuracy:
+            max_accuracy = accuracy
+            lambda_values = lamdas
+            q_values = log_values
 
     return lambda_values, q_values
 
-#INCOMPLETE FUNCTION
-# def calculate_accuracy(tags, brown_dev_words):
-    """ Function to calculate the accuracy of the Viterbi algorithm by comparing the output of the POS tagger to the actual tags
-        provided in each validation set. """
-
-
-
-
-        # return accuracy
-
+#might need to modify this function
 def transition_probs(taglists, unigrams, bigrams, trigrams, lambdas):
     """ Function to find log transition probabilities for each POS trigram in the training corpus using deleted interpolation.
         It returns a dictionary containing the corpus' log transition probabilities for each POS trigram present in the training corpus.
@@ -77,9 +81,19 @@ def transition_probs(taglists, unigrams, bigrams, trigrams, lambdas):
     trigram_p = {(a, b, c): math.log(trigrams[(a, b, c)], 2) - math.log(bigrams[(a, b)], 2) for a, b, c in trigrams}
 
     #calculating log transmission probabilities
-    q_values = {(a, b , c): math.log(lambdas[0] * (2**trigram_p[(a, b, c)]) + lambda[1] * (2**bigram_p[(b, c)]) + lambda[2] * (2**unigram_p[(c,)]), 2) for a, b, c in trigram_p}
+    q_values = {(a, b , c): math.log(lambdas[0] * (2**trigram_p[(a, b, c)]) + lambdas[1] * (2**bigram_p[(b, c)]) + lambdas[2] * (2**unigram_p[(c,)]), 2) for a, b, c in trigram_p}
 
     return q_values
+
+#INCOMPLETE FUNCTION
+# def calculate_accuracy(tags, test_set):
+    """ Function to calculate the accuracy of the Viterbi algorithm by comparing the output of the POS tagger to the actual tags
+        provided in each validation set. """
+
+
+
+
+        # return accuracy
 
 if __name__ == '__main__':
 
@@ -91,14 +105,18 @@ if __name__ == '__main__':
 
     tokenlists = pickle.load(open(output_path + "tokenlists.pickle", "rb" ))
     taglists = pickle.load(open(output_path + "taglists.pickle", "rb" ))
-    e_values = pickle.load(open(output_path + "e_probs.pickle", "rb" ))
+    e_values = dict(pickle.load(open(output_path + "e_probs.pickle", "rb" )))
     known_words = pickle.load(open(output_path + "known_words.pickle", "rb" ))
     pos_set = pickle.load(open(output_path + "pos_set.pickle", "rb" ))
 
-    lambda_values, q_values = find_lambdas(taglists, unigrams, bigrams, trigrams)
+    candidate_values = lambda_candidates(0.001, 1, 0.001)
+
+    # lambda_values, q_values = find_lambdas(candidate_values, taglists, unigrams, bigrams, trigrams, e_values)
     # print(q_probs)
 
+    candidate_values = pickle.dump(candidate_values, open(output_path + "candidate_values.pickle", "wb"))
     # q_probs = pickle.dump(q_probs, open(output_path + "q_probs.pickle", "wb" ))
+    # lambda_values = pickle.dump(lambda_values, open(output_path + "lamnda_values.pickle", "wb" ))
 
     finish = time.perf_counter()
     print(f'Finished in {round(finish-start, 2)} second(s)')
