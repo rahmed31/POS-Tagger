686a39541f6049a2eae4343fc07f9d91d0f82b1b
Raihan Ahmed <raihan.ahmed117@gmail.com>
2021-12-01 17:13:51 -0600

    moved accuracy function to new file

---
+++ accuracy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------------------
+#The purpose of this file is to calculate the accuracy of the POS tagging model
+#
+#Copyright (C) 2021, released under MIT License
+#Author: Raihan Ahmed, Chicago, IL
+#email: rahmed10@neiu.edu
+#-------------------------------------------------------------------------------------------
+
+#INCOMPLETE FUNCTION
+def calculate_accuracy(tags, test_set):
+    """ Function to calculate the accuracy of the Viterbi algorithm by comparing the output of the POS tagger to the actual tags
+        provided in each validation set. """
+
+        num_correct = 0
+        total = 0
+
+
+
+
+
+        return round(num_correct/float(total), 3)
