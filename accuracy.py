f9777c15e396bcc6a9464f40c8ece53d7c40193e
Raihan Ahmed <raihan.ahmed117@gmail.com>
2021-12-02 00:45:11 -0600

    renamed some variables

--- accuracy.py
+++ accuracy.py
@@ -19,14 +19,16 @@ def calculate_accuracy(test_tags, model_tags):
     num_correct = 0
     total = 0
 
-    for test_list, model_list in zip(test_tags, model_tags):
-        for test_pos, model_pos in zip(test_list, model_list):
+    for test_taglist, model_taglist in zip(test_tags, model_tags):
+        for test_pos, model_pos in zip(test_taglist, model_taglist):
             if test_pos == model_pos:
                 num_correct += 1
 
             total += 1
 
-    return round(num_correct/float(total), 3) * 100
+    accuracy = round(num_correct/float(total), 3) * 100
+
+    return accuracy
 
 if __name__ == '__main__':
 
